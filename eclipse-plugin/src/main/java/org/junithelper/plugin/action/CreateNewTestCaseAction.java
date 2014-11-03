244b1e2bd739bd15785a912e474b63c0a50c171d
hunthu2000 <huxiegang@gmail.com>
2014-11-03 15:05:19 +0800

    bugfix for "should use project not workspace Absolute Path"

--- eclipse-plugin/src/main/java/org/junithelper/plugin/action/CreateNewTestCaseAction.java
+++ eclipse-plugin/src/main/java/org/junithelper/plugin/action/CreateNewTestCaseAction.java
@@ -102,8 +102,9 @@ public class CreateNewTestCaseAction extends AbstractAction implements IActionDe
             // get project path, resource path
             projectName = getProjectName(structuredSelection);
             resourcePathForTargetClassFile = getResourcePathForTargetClassFile(structuredSelection);
-            String projectRootAbsolutePath = getWorkspaceRootAbsolutePath(getIWorkspaceRoot())
-                    + StringValue.DirectorySeparator.General + projectName + StringValue.DirectorySeparator.General;
+//            String projectRootAbsolutePath = getWorkspaceRootAbsolutePath(getIWorkspaceRoot())
+//                    + StringValue.DirectorySeparator.General + projectName + StringValue.DirectorySeparator.General;
+            String projectRootAbsolutePath = getIProject(projectName).getLocation() + StringValue.DirectorySeparator.General;
 
             resourcePathForTestClassFile = resourcePathForTargetClassFile.replaceFirst(
                     config.directoryPathOfProductSourceCode, config.directoryPathOfTestSourceCode).replace(
